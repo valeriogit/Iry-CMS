37b3367505c36b91124832bb91e8bf329e86f5d7
valeriogit <valerio.palazzo@gmail.com>
2021-01-09 12:52:37 +0100

    fix

--- app/Http/Plugins/Database/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Http\Plugins;
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Plugins\Database\Controller\ControllerDB;
-
-Route::get('/admin/db', [ControllerDB::class, 'indexx']);
